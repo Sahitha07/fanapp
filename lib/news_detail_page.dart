5e9324ca98099cf17f09410c3c643447850c4582
Sahitha07 <140141065+Sahitha07@users.noreply.github.com>
2023-09-13 16:55:12 +0900

    Update news_detail_page.dart

--- lib/news_detail_page.dart
+++ lib/news_detail_page.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class NewsDetailPage extends StatelessWidget {
-  final String title;
-  final String content;
-  final String link;
-
-  NewsDetailPage({required this.title, required this.content, required this.link});
+class EventsPage extends StatelessWidget {
+  final List<String> events = List.generate(10, (index) => 'Event ${index + 1}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('News Detail'),
-        backgroundColor: Color(0xFF2C142E),
-      ),
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -24,33 +15,78 @@ class NewsDetailPage extends StatelessWidget {
             colors: [Colors.black, Color(0xFF2C142E)],
           ),
         ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  content,
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement redirection logic to the provided link
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
+        child: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (BuildContext context, int index) {
+            final eventName = events[index];
+            return ListTile(
+              title: Text(eventName, style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EventDetailsPage(eventName: eventName),
                   ),
-                  child: Text('Read More', style: TextStyle(color: Color(0xFF2C142E))),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class EventDetailsPage extends StatelessWidget {
+  final String eventName;
+
+  EventDetailsPage({required this.eventName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(eventName),
+        backgroundColor: Color(0xFF2C142E),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black, Color(0xFF2C142E)],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                ' $eventName',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'More detailed information about the event goes here.',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  const url = 'https://www.youtube.com/playlist?list=PL5hrGMysD_GvC9VL7eeXG4j0apdqvh4lA'; // Replace with your event link
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
                 ),
-              ],
-            ),
+                child: Text('Visit Event Link', style: TextStyle(color: Colors.black)),
+              ),
+            ],
           ),
         ),
       ),
